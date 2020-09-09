0bccdcfb134455392d4607af396d2e274ac85916
Edmara <edmaracav@gmail.com>
2020-09-08 21:20:03 -0300

    Configuração do contador

--- web-app/src/app/contador/contador.component.ts
+++ web-app/src/app/contador/contador.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContadorComponent implements OnInit {
 
+  public contador = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  contarClique(){
+    this.contador = this.contador + 1 ;
   }
 
 }
