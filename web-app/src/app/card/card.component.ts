05c73b9a23398025b23bbf720d377e2bfd3c03a3
Edmara <edmaracav@gmail.com>
2020-10-24 10:56:34 -0300

    add card

--- web-app/src/app/card/card.component.ts
+++ web-app/src/app/card/card.component.ts
@@ -13,6 +13,29 @@ export class CardComponent implements OnInit {
   public usuario = 'Edmara Cavalcante';
   public profissao = 'Engenheira de Software';
 
+  public lista = [
+
+    {
+      id:'1',
+      year: '2007',
+      cert: 'SCWCD 1.5'
+    },
+    {
+      id:'2',
+      year: '2009',
+      cert: 'SCWCD 1.5'
+    },
+    {
+      id:'3',
+      year: '2011',
+      cert: 'CTFL'
+    }
+  ];
+
+
+
+
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ web-app/src/app/diretivas/diretivas.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DiretivasComponent } from './diretivas.component';
+
+
+describe('DiretivasComponent', () => {
+  let component: DiretivasComponent;
+  let fixture: ComponentFixture<DiretivasComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DiretivasComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DiretivasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
