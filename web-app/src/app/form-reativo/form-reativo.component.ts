8ba989a5a457e8a6dc127a46194c0eba3b8fb481
Edmara <edmaracav@gmail.com>
2020-10-24 17:59:02 -0300

    add validadores

--- web-app/src/app/form-reativo/form-reativo.component.ts
+++ web-app/src/app/form-reativo/form-reativo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reativo',
@@ -8,9 +8,47 @@ import { NgForm } from '@angular/forms';
 })
 export class FormReativoComponent implements OnInit {
 
-  constructor() { }
+  formCadastro : FormGroup;
 
+  constructor(private fb : FormBuilder) {
+    this.formCadastro = new FormGroup({
+      'nome': new FormControl(null, Validators.required),
+      'email': new FormControl(null, Validators.required),
+      'telefone': new FormControl(null, Validators.required),
+    });
+  }
+
+  criarFormularioDeUsuario(){
+    this.formCadastro = this.fb.group({
+      nome: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.min(5),
+          Validators.maxLength(150)
+        ])
+      ],
+      email: ['', Validators.compose([Validators.email])],
+      cpf: ['', Validators.compose([Validators.required])],
+      nascimento: ['', Validators.compose([Validators.required])],
+      senha: [
+        '', 
+        Validators.compose([
+          Validators.required, Validators.min(8)])],
+      confirmarSenha: ['', Validators.compose([Validators.required])],
+
+    });
+  }
   ngOnInit(): void {
+  }
+
+  enviar(){
+    let dados = `
+      Nome: ${this.formCadastro.value.nome}
+      Email: ${this.formCadastro.value.email}
+      Telefone: ${this.formCadastro.value.telefone}
+    `;
+    console.log(dados);
   }
 
 }
